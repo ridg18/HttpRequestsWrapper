aca4fa234eae961e6b2ea462a6fe8b8d6add41eb
ragas <ridg18@gmail.com>
2015-04-21 11:03:14 +0300

    Add Tests

--- src/test/java/DeleteRequestTest.java
+++ src/test/java/DeleteRequestTest.java
@@ -1,3 +1,9 @@
+import com.httpRequests.requests.DeleteRequest;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ragas
@@ -6,4 +12,18 @@
  * To change this template use File | Settings | File Templates.
  */
 public class DeleteRequestTest {
+    private String url = "http://httpbin.org/delete";
+    private static HashMap<String,String> headers = new HashMap<String, String>();
+    static {
+        headers.put("Accept","application/json");
+        headers.put("Content-Type","application/json");
+    }
+
+    @Test
+    public void deleteRequestTest1() throws Exception {
+
+        JSONObject getJsonResponse = new DeleteRequest().withURL(url)
+                .withHeaders(headers).sendReqAndGetResAsJSONObject();
+        System.out.println(getJsonResponse);
+    }
 }
--- src/test/java/PutRequestTest.java
+++ src/test/java/PutRequestTest.java
@@ -1,3 +1,9 @@
+import com.httpRequests.requests.PutRequest;
+import org.json.JSONObject;
+import org.junit.Test;
+
+import java.util.HashMap;
+
 /**
  * Created with IntelliJ IDEA.
  * User: ragas
@@ -6,4 +12,20 @@
  * To change this template use File | Settings | File Templates.
  */
 public class PutRequestTest {
+    private String url = "http://httpbin.org/put";
+    private String url2 = "https://httpbin.org/get?show_env=1";
+    private String requestBody = "custname=qwe&custtel=123&custemail=123%40qsd.com&size=small&topping=bacon&delivery=13%3A45&comments=asdasdasd";
+    private static HashMap<String,String> headers = new HashMap<String, String>();
+    static {
+        headers.put("Accept","application/json");
+        headers.put("Content-Type","application/json");
+    }
+
+    @Test
+    public void putRequestTest1() throws Exception {
+
+        JSONObject getJsonResponse = new PutRequest().withBody(requestBody).withURL(url)
+                .withHeaders(headers).sendReqAndGetResAsJSONObject();
+        System.out.println(getJsonResponse);
+    }
 }
